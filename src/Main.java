ce28026e375b82de30ac4ce925e3f5b7e57e06b0
Gustavo Santana <gsb.jmt@gmail.com>
2025-03-21 23:12:48 -0400

    add one error

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello World!");
     }
-}
+}gi
